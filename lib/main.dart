e75f8658682439e57786d5f6deb958f6558024c4
hermanlyx <hermanlyx@gmail.com>
2022-12-27 23:28:48 +0800

    drawer, listView, garbage collection

--- lib/main.dart
+++ lib/main.dart
@@ -11,60 +11,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text('Flutter is Fun!'),
-      ),
-      // body: Center(
-      //   child: Container(
-      //     child: const Text('Hi Mom'),
-      //     margin: const EdgeInsets.all(10),
-      //     padding: const EdgeInsets.all(10),
-      //     // decoration: ,
-      //   ),
-      // ),
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              title: const Text('Flutter is Fun!'),
+            ),
+            // body: Center(
+            //   child: Container(
+            //     child: const Text('Hi Mom'),
+            //     margin: const EdgeInsets.all(10),
+            //     padding: const EdgeInsets.all(10),
+            //     // decoration: ,
+            //   ),
+            // ),
 
-      // body: Row(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     crossAxisAlignment: CrossAxisAlignment.end,
-      //     children: const [
-      //       Expanded(
-      //         flex: 4,
-      //         child: Icon(Icons.backpack),
-      //       ),
-      //       Icon(Icons.leaderboard),
-      //       Icon(Icons.person)
-      //     ]),
+            // body: Row(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     crossAxisAlignment: CrossAxisAlignment.end,
+            //     children: const [
+            //       Expanded(
+            //         flex: 4,
+            //         child: Icon(Icons.backpack),
+            //       ),
+            //       Icon(Icons.leaderboard),
+            //       Icon(Icons.person)
+            //     ]),
 
-      body: Stack(
-        children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-          ),
-          // Positioned(
-          //   child: Icon(Icons.verified),
-          //   top: 25,
-          //   left: 25,
-          // ),
-          const Align(
-            alignment: Alignment.center,
-            child: Icon(Icons.verified),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print("pressed!");
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.business), label: "Business"),
-        BottomNavigationBarItem(icon: Icon(Icons.school), label: "School"),
-      ]),
-    ));
+            body: ListView(
+              scrollDirection: Axis.horizontal,
+              addAutomaticKeepAlives: false, //garbage collection
+              children: [
+                Container(
+                  color: Colors.red,
+                ),
+                Container(
+                  color: Colors.blue,
+                ),
+                Container(
+                  color: Colors.green,
+                )
+              ],
+            )
+            // Stack(
+            //   children: [
+            //     Container(
+            //       color: Colors.red,
+            //       width: 100,
+            //       height: 100,
+            //     ),
+            //     // Positioned(
+            //     //   child: Icon(Icons.verified),
+            //     //   top: 25,
+            //     //   left: 25,
+            //     // ),
+            //     const Align(
+            //       alignment: Alignment.center,
+            //       child: Icon(Icons.verified),
+            //     ),
+            //   ],
+            // ),
+
+            // floatingActionButton: FloatingActionButton(
+            //   child: const Icon(Icons.add),
+            //   onPressed: () {
+            //     print("pressed!");
+            //   },
+            // ),
+
+            // bottomNavigationBar: BottomNavigationBar(items: const [
+            //   BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            //   BottomNavigationBarItem(icon: Icon(Icons.business), label: "Business"),
+            //   BottomNavigationBarItem(icon: Icon(Icons.school), label: "School"),
+            // ]),
+
+            // drawer: const Drawer(
+            //   child: Text('Yo!'),
+            // ),
+            ));
   }
 }
+
+//? Why do some variables require const and some don't?
